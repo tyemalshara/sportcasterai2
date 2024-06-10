edcea57848613210585118c5469f96bcc8663c1d
Taim <84383288+tyemalshara@users.noreply.github.com>
2024-06-10 23:40:39 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ from PIL import Image
 from sklearn.cluster import KMeans
 import streamlit as st
 import streamlit_authenticator as stauth
+import yaml
+from yaml.loader import SafeLoader
 # Essential custom fuctions for the app to run
 from main_functions import *
 
@@ -23,6 +25,78 @@ st.set_page_config(page_title="SportCasterAI - Goal Predictor",
                           'About': "mailto:s190234@th-ab.de"
                               }
                   )
+# Creating a login widget
+with open('../config.yaml') as file:
+    config = yaml.load(file, Loader=SafeLoader)
+authenticator = stauth.Authenticate(
+    config['credentials'],
+    config['cookie']['name'],
+    config['cookie']['key'],
+    config['cookie']['expiry_days'],
+    config['pre-authorized']
+)
+# Authenticating users
+authenticator.login()
+if st.session_state["authentication_status"]:
+    authenticator.logout()
+    st.write(f'Welcome *{st.session_state["name"]}*')
+    st.title('Some content')
+elif st.session_state["authentication_status"] is False:
+    st.error('Username/password is incorrect')
+elif st.session_state["authentication_status"] is None:
+    st.warning('Please enter your username and password')
+# Creating a reset password widget
+if st.session_state["authentication_status"]:
+    try:
+        if authenticator.reset_password(st.session_state["username"]):
+            st.success('Password modified successfully')
+            with open('../config.yaml', 'w') as file:
+              yaml.dump(config, file, default_flow_style=False)
+    except Exception as e:
+        st.error(e)
+# Creating a new user registration widget
+try:
+    email_of_registered_user, username_of_registered_user, name_of_registered_user = authenticator.register_user(pre_authorization=False)
+    if email_of_registered_user:
+        st.success('User registered successfully')
+        with open('../config.yaml', 'w') as file:
+          yaml.dump(config, file, default_flow_style=False)
+except Exception as e:
+    st.error(e)
+# Creating a forgot password widget
+try:
+    username_of_forgotten_password, email_of_forgotten_password, new_random_password = authenticator.forgot_password()
+    if username_of_forgotten_password:
+        st.success('New password to be sent securely')
+        # The developer should securely transfer the new password to the user.
+        with open('../config.yaml', 'w') as file:
+          yaml.dump(config, file, default_flow_style=False)
+    elif username_of_forgotten_password == False:
+        st.error('Username not found')
+except Exception as e:
+    st.error(e)
+# Creating a forgot username widget
+try:
+    username_of_forgotten_username, email_of_forgotten_username = authenticator.forgot_username()
+    if username_of_forgotten_username:
+        st.success('Username to be sent securely')
+        # The developer should securely transfer the username to the user.
+        with open('../config.yaml', 'w') as file:
+          yaml.dump(config, file, default_flow_style=False)
+    elif username_of_forgotten_username == False:
+        st.error('Email not found')
+except Exception as e:
+    st.error(e)
+# Creating an update user details widget
+if st.session_state["authentication_status"]:
+    try:
+        if authenticator.update_user_details(st.session_state["username"]):
+            st.success('Entries updated successfully')
+            with open('../config.yaml', 'w') as file:
+              yaml.dump(config, file, default_flow_style=False)
+    except Exception as e:
+        st.error(e)
+  
 # # Hiding menu and footer
 # hide_default_format = """
 #        <style>
