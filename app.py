9b8565cb1198730edcbddf9d7a780d1053922801
Taim <84383288+tyemalshara@users.noreply.github.com>
2024-06-10 23:15:00 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import numpy as np
 from PIL import Image
 from sklearn.cluster import KMeans
 import streamlit as st
+import streamlit_authenticator as stauth
 # Essential custom fuctions for the app to run
 from main_functions import *
 
