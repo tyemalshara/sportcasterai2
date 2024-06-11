02289f0b0287e8726b252eb81a55e202bacc36fa
Taim <84383288+tyemalshara@users.noreply.github.com>
2024-06-11 09:05:44 +0200

    Update app.py

--- app.py
+++ app.py
@@ -39,12 +39,100 @@ authenticator = stauth.Authenticate(
 authenticator.login()
 if st.session_state["authentication_status"]:
     authenticator.logout()
+    # Creating an update user details widget
+    if st.session_state["authentication_status"]:
+        try:
+            if authenticator.update_user_details(st.session_state["username"]):
+                st.success('Entries updated successfully')
+                with open('config.yaml', 'w') as file:
+                  yaml.dump(config, file, default_flow_style=False)
+        except Exception as e:
+            st.error(e) 
     st.write(f'Welcome *{st.session_state["name"]}*')
     st.title('Some content')
+    st.header(':grey[Welcome to] :orange[SportCasterAI] - :blue[Goal Predictor]', divider='rainbow')
+    YT_URL = st.text_input("Enter YouTube Live URL", placeholder="https://www.youtube.com/watch?v=LTdT9BkW77k")
+    if YT_URL != "":
+      st.video(YT_URL, format="video/mp4", autoplay=True, start_time=0)
+      stream_url = stream_to_url(YT_URL, "best")
+      if stream_url:  # check if stream_url is not None
+        cap = cv2.VideoCapture(stream_url)
+        success, frame = cap.read()
+        if not success:
+          print("Error: Can't receive frame (stream end?). Exiting ...")
+        col1, col2 = st.columns(2)
+        col1.image(frame, caption='This image is taken from the YouTube live stream. ', channels="BGR")
+        cap.release()
+        # cv2.destroyAllWindows()
+        cv2.imwrite("UserImage.jpg", frame)
+        st.session_state.input_file_path = "UserImage.jpg"
+        # Read image
+        frame = cv2.imread('UserImage.jpg')
+        # display the image
+        # st.image(frame, caption='Again! This is your image. It has been successfully uploaded.', channels="BGR")
+        # add a button to prompt a prediction
+        if col2.button("Predict"):
+          # Run the prediction
+          # add a spinner to the UI
+          with st.spinner('Prediction is being processed, results could take a few seconds to 10 minutes...'):
+            # Run the workflow on current frame -> wf.run_on(array=frame)
+            # Fetch the results from Ikomia's API
+            response_uuid, JWT = call_IkomiaAPI()
+            results = fetch_workflow_results(response_uuid, JWT)
+            if results:
+                try:
+                  print("Workflow results:", results)
+                  instance_segmentation_objects = results[1]['INSTANCE_SEGMENTATION']['detections']  # instance_segmentation.get_results().get_objects()
+                  object_detection_objects = results[0]['OBJECT_DETECTION']['detections'] # object_detection.get_results().get_objects()
+                  player_id_in_crossing_zone, player_id_in_recipient_zone, player_id_in_pitch = players_crossing_zones(instance_segmentation_objects, object_detection_objects, frame)
+                  player_with_ball = player_near_ball(object_detection_objects)
+                  player_team_1, player_team_2, player_team_1_in_crossing_zone, player_team_1_in_recipient_zone, player_team_1_in_pitch, player_team_2_in_crossing_zone, player_team_2_in_recipient_zone, player_team_2_in_pitch = team_detection(frame, instance_segmentation_objects)
+                  df = InitFrameDataDataFrame(player_id_in_crossing_zone, player_id_in_recipient_zone, player_id_in_pitch, player_with_ball, len(player_team_1), len(player_team_2), player_team_1_in_crossing_zone, player_team_1_in_recipient_zone, player_team_1_in_pitch, player_team_2_in_crossing_zone, player_team_2_in_recipient_zone, player_team_2_in_pitch, frame)
+    
+                  GoalPredResults = PredictGoal(df)
+                  col2.write(GoalPredResults)
+                except Exception as e:
+                    print(e)
+                    st.error("Something went wrong. Please try again!")
+            else:
+                print("Failed to retrieve results within the given timeout.")
 elif st.session_state["authentication_status"] is False:
     st.error('Username/password is incorrect')
+    # Creating a forgot password widget
+    try:
+        username_of_forgotten_password, email_of_forgotten_password, new_random_password = authenticator.forgot_password()
+        if username_of_forgotten_password:
+            st.success('New password to be sent securely')
+            # The developer should securely transfer the new password to the user.
+            with open('config.yaml', 'w') as file:
+              yaml.dump(config, file, default_flow_style=False)
+        elif username_of_forgotten_password == False:
+            st.error('Username not found')
+    except Exception as e:
+        st.error(e)
+    # Creating a forgot username widget
+    try:
+        username_of_forgotten_username, email_of_forgotten_username = authenticator.forgot_username()
+        if username_of_forgotten_username:
+            st.success('Username to be sent securely')
+            # The developer should securely transfer the username to the user.
+            with open('config.yaml', 'w') as file:
+              yaml.dump(config, file, default_flow_style=False)
+        elif username_of_forgotten_username == False:
+            st.error('Email not found')
+    except Exception as e:
+        st.error(e)
 elif st.session_state["authentication_status"] is None:
-    st.warning('Please enter your username and password')
+    st.warning('Please enter your username and password. New member? Register now!')
+    # Creating a new user registration widget
+    try:
+        email_of_registered_user, username_of_registered_user, name_of_registered_user = authenticator.register_user(pre_authorization=False)
+        if email_of_registered_user:
+            st.success('User registered successfully')
+            with open('config.yaml', 'w') as file:
+              yaml.dump(config, file, default_flow_style=False)
+    except Exception as e:
+        st.error(e)
 # Creating a reset password widget
 if st.session_state["authentication_status"]:
     try:
@@ -54,49 +142,10 @@ if st.session_state["authentication_status"]:
               yaml.dump(config, file, default_flow_style=False)
     except Exception as e:
         st.error(e)
-# Creating a new user registration widget
-try:
-    email_of_registered_user, username_of_registered_user, name_of_registered_user = authenticator.register_user(pre_authorization=False)
-    if email_of_registered_user:
-        st.success('User registered successfully')
-        with open('config.yaml', 'w') as file:
-          yaml.dump(config, file, default_flow_style=False)
-except Exception as e:
-    st.error(e)
-# Creating a forgot password widget
-try:
-    username_of_forgotten_password, email_of_forgotten_password, new_random_password = authenticator.forgot_password()
-    if username_of_forgotten_password:
-        st.success('New password to be sent securely')
-        # The developer should securely transfer the new password to the user.
-        with open('config.yaml', 'w') as file:
-          yaml.dump(config, file, default_flow_style=False)
-    elif username_of_forgotten_password == False:
-        st.error('Username not found')
-except Exception as e:
-    st.error(e)
-# Creating a forgot username widget
-try:
-    username_of_forgotten_username, email_of_forgotten_username = authenticator.forgot_username()
-    if username_of_forgotten_username:
-        st.success('Username to be sent securely')
-        # The developer should securely transfer the username to the user.
-        with open('config.yaml', 'w') as file:
-          yaml.dump(config, file, default_flow_style=False)
-    elif username_of_forgotten_username == False:
-        st.error('Email not found')
-except Exception as e:
-    st.error(e)
-# Creating an update user details widget
-if st.session_state["authentication_status"]:
-    try:
-        if authenticator.update_user_details(st.session_state["username"]):
-            st.success('Entries updated successfully')
-            with open('config.yaml', 'w') as file:
-              yaml.dump(config, file, default_flow_style=False)
-    except Exception as e:
-        st.error(e)
-  
+
+
+
+
 # # Hiding menu and footer
 # hide_default_format = """
 #        <style>
@@ -115,50 +164,4 @@ if st.session_state["authentication_status"]:
 # st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
 # Streamlit UI elements
-st.header(':grey[Welcome to] :orange[SportCasterAI] - :blue[Goal Predictor]', divider='rainbow')
-YT_URL = st.text_input("Enter YouTube Live URL", placeholder="https://www.youtube.com/watch?v=LTdT9BkW77k")
 
-if YT_URL != "":
-  st.video(YT_URL, format="video/mp4", autoplay=True, start_time=0)
-  stream_url = stream_to_url(YT_URL, "best")
-  if stream_url:  # check if stream_url is not None
-    cap = cv2.VideoCapture(stream_url)
-    success, frame = cap.read()
-    if not success:
-      print("Error: Can't receive frame (stream end?). Exiting ...")
-    col1, col2 = st.columns(2)
-    col1.image(frame, caption='This image is taken from the YouTube live stream. ', channels="BGR")
-    cap.release()
-    # cv2.destroyAllWindows()
-    cv2.imwrite("UserImage.jpg", frame)
-    st.session_state.input_file_path = "UserImage.jpg"
-    # Read image
-    frame = cv2.imread('UserImage.jpg')
-    # display the image
-    # st.image(frame, caption='Again! This is your image. It has been successfully uploaded.', channels="BGR")
-    # add a button to prompt a prediction
-    if col2.button("Predict"):
-      # Run the prediction
-      # add a spinner to the UI
-      with st.spinner('Prediction is being processed, results could take a few seconds to 10 minutes...'):
-        # Run the workflow on current frame -> wf.run_on(array=frame)
-        # Fetch the results from Ikomia's API
-        response_uuid, JWT = call_IkomiaAPI()
-        results = fetch_workflow_results(response_uuid, JWT)
-        if results:
-            try:
-              print("Workflow results:", results)
-              instance_segmentation_objects = results[1]['INSTANCE_SEGMENTATION']['detections']  # instance_segmentation.get_results().get_objects()
-              object_detection_objects = results[0]['OBJECT_DETECTION']['detections'] # object_detection.get_results().get_objects()
-              player_id_in_crossing_zone, player_id_in_recipient_zone, player_id_in_pitch = players_crossing_zones(instance_segmentation_objects, object_detection_objects, frame)
-              player_with_ball = player_near_ball(object_detection_objects)
-              player_team_1, player_team_2, player_team_1_in_crossing_zone, player_team_1_in_recipient_zone, player_team_1_in_pitch, player_team_2_in_crossing_zone, player_team_2_in_recipient_zone, player_team_2_in_pitch = team_detection(frame, instance_segmentation_objects)
-              df = InitFrameDataDataFrame(player_id_in_crossing_zone, player_id_in_recipient_zone, player_id_in_pitch, player_with_ball, len(player_team_1), len(player_team_2), player_team_1_in_crossing_zone, player_team_1_in_recipient_zone, player_team_1_in_pitch, player_team_2_in_crossing_zone, player_team_2_in_recipient_zone, player_team_2_in_pitch, frame)
-
-              GoalPredResults = PredictGoal(df)
-              col2.write(GoalPredResults)
-            except Exception as e:
-                print(e)
-                st.error("Something went wrong. Please try again!")
-        else:
-            print("Failed to retrieve results within the given timeout.")
