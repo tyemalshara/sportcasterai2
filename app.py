ee7329cf8e64879ca4ba3e5f5e9bdf774e906ae0
Taim <84383288+tyemalshara@users.noreply.github.com>
2024-06-12 10:31:46 +0200

    Update app.py

--- app.py
+++ app.py
@@ -45,12 +45,21 @@ if st.session_state["authentication_status"]:
     # Creating an update user details widget
     if st.session_state["authentication_status"]:
         try:
-            if authenticator.update_user_details(st.session_state["username"]):
+            if authenticator.update_user_details(st.session_state["username"], 'sidebar'):
                 st.success('Entries updated successfully')
                 with open('config.yaml', 'w') as file:
                   yaml.dump(config, file, default_flow_style=False)
         except Exception as e:
             st.error(e) 
+    # Creating a reset password widget
+    if st.session_state["authentication_status"]:
+        try:
+            if authenticator.reset_password(st.session_state["username"], 'sidebar'):
+                st.success('Password modified successfully')
+                with open('config.yaml', 'w') as file:
+                  yaml.dump(config, file, default_flow_style=False)
+        except Exception as e:
+            st.error(e)
     st.write(f'Welcome *{st.session_state["name"]}*')
     st.title('Some content')
     st.header(':grey[Welcome to] :orange[SportCasterAI] - :blue[Goal Predictor]', divider='rainbow')
@@ -189,15 +198,7 @@ elif st.session_state["authentication_status"] is None:
               yaml.dump(config, file, default_flow_style=False)
     except Exception as e:
         st.error(e)
-# Creating a reset password widget
-if st.session_state["authentication_status"]:
-    try:
-        if authenticator.reset_password(st.session_state["username"]):
-            st.success('Password modified successfully')
-            with open('config.yaml', 'w') as file:
-              yaml.dump(config, file, default_flow_style=False)
-    except Exception as e:
-        st.error(e)
+
 
 
 
